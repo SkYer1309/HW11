d01c8f1480f4da10d7c9ef52ff99630b675fc5ed
skyer <sky13net84@gmail.com>
2026-03-11 18:06:33 +0300

    Первый коммит

---
+++ src/Main.java
@@ -0,0 +1,37 @@
+public class Main {
+    public static void main(String[] args) {
+        checkYear(2023);
+        checkDeviceOS(0, 2020);
+        //task3();
+    }
+
+    public static void checkYear(int year) {
+        System.out.println("Задача 1");
+        if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
+            System.out.println(year + " год является високосным");
+        } else {
+            System.out.println(year + " год не является високосным");
+        }
+    }
+    public  static  void checkDeviceOS(int clientOS, int DeviceYear){
+        System.out.println("Задача 2");
+        if (clientOS == 0) {
+            System.out.println("Установите версию приложения для iOS по ссылке:");
+        } else if (clientOS == 1) {
+            System.out.println("Установите версию приложения для Android  по ссылке:");
+        }
+        if (clientOS == 0 && DeviceYear < 2015) {
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке:");
+        } else if (clientOS == 1 && DeviceYear < 2015) {
+            System.out.println("Установите облегченную версию приложения для Android по ссылке:");
+        } else {
+            System.out.println("Установите версию приложения для " + (clientOS == 0 ? "iOS" : "Android")
+                    + " по ссылке:");
+        }
+    }
+    }
+
+
+
+
+
